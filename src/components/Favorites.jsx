d38ff4c7fe732124a8d19a0d08bee7f029612ffc
Camille Sun <cooclle@hotmail.com>
2020-04-21 16:08:53 -0700

    add usestate in <favorite>

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,31 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import {BmCard} from "./MyCard";
 
 function Favorites(props) {
+  const [cardNum, setCardNum] = useState(props.BmContent.length);
+  function changeCardNum(newValue) {
+    setCardNum(newValue);
+  }
+  function decCardNum(){
+    setCardNum(prev=>prev-1);
+  }
     return (
       <div>
-      <div className='indicator'>Favorites</div>
-        {props.BmContent.length === 0 ?
-          <p>You have no saved articles</p>
-          :
-          props.BmContent.map((info, index) => {
-            return (
-              <BmCard
-                onLoading={props.onLoading}
-                switchst={info.switchst}
-                changeContent={props.changeContent}
-                removeBmContent={props.removeBmContent}
-                key={index}
-                id={info.id}
-                url={info.url}
-                title={info.title}
-                section={info.section}
-                image={info.image}
-                date={info.date}
-              />
-          )})
+        {cardNum !== 0 ? 
+        <div className='indicator' >Favorites</div> :
+        <p className='no-save-ind' style={{textAlign:'center',fontSize:'1.5rem'}}>You have no saved articles</p>
         }
-      </div>
+            {props.BmContent.map((info, index) => {
+              return (
+                <BmCard
+                  onLoading={props.onLoading}
+                  switchst={info.switchst}
+                  changeContent={props.changeContent}
+                  BmContent={props.BmContent}
+                  removeBmContent={props.removeBmContent}
+                  decCardNum={decCardNum}
+                  key={index}
+                  id={info.id}
+                  url={info.url}
+                  title={info.title}
+                  section={info.section}
+                  image={info.image}
+                  date={info.date}
+                />
+            )})}
+          </div>
+      
     );
 }
 
