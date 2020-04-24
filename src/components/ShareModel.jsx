64fef5e98f532ac7db036bdbaedcc94ead22e154
xingyu <xyyimian@mail.ustc.edu.cn>
2020-04-23 21:35:59 -0700

    sharemodel css

--- src/components/ShareModel.jsx
+++ src/components/ShareModel.jsx
@@ -26,21 +26,21 @@ function ShareModel(props){
             {close => {return (
                 <div className="mymodal">
                     <a className='close' onClick={close}>&times;</a>
-                    <div className='content truncateDescription2' style={{fontSize:'1rem',width:'370px'}}>{props.title}</div>
+                    <div className='content truncateDescription2 ShMod-title'>{props.title}</div>
                     <hr />
-                    <div className='header' style={{fontSize:'1rem',textAlign:'center'}}>Share via</div>
+                    <div className='header ShMod-via'>Share via</div>
                     <div>
-                        <div style={{padding:'0 40px', display:'inline'}}>
+                        <div className="ShMod-icon il">
                         <FacebookShareButton url={props.url} quote={'#' + hashtag} children="">
                             <FacebookIcon size={50} round={true} />
                         </FacebookShareButton>
                         </div>
-                        <div style={{padding: '0 40px', display:'inline' }}>
+                        <div className="ShMod-icon il">
                         <TwitterShareButton url={props.url} hashtags={[hashtag]} children="">
                             <TwitterIcon size={50} round={true} />
                         </TwitterShareButton>
                         </div>
-                        <div style={{padding: '0 40px', display:'inline' }}>
+                        <div className="ShMod-icon il">
                         <EmailShareButton subject={'#' + hashtag} url={props.url}>
                             <EmailIcon size={50} round={true} />
                         </EmailShareButton>       
